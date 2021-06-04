8cf38c0465ae2b051e3a4e7f41fd4c9e7eae850b
Matthew Starcher <mlstarcher@gmail.com>
2021-06-04 15:27:24 -0700

    added click handlers to emit play and stop messages from buttons

--- client/src/components/PlaybackControls.js
+++ client/src/components/PlaybackControls.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
 export default function PlaybackControls({ socket }) {
+  const handlePlay = () => {
+    socket.emit('play', 'play')
+  }
+
+  const handleStop = () => {
+    socket.emit('stop', 'stop')
+  }
+
     return (
       <div>
-        <button>Play</button>
-        <button>Stop</button>
+        <button onClick={handlePlay}>Play</button>
+        <button onClick={handleStop}>Stop</button>
       </div>
     )
 }
