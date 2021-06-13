c0e2daec6820cd164decaaddd86f7b9a7df15e55
Matthew Starcher <mlstarcher@gmail.com>
2021-06-12 18:26:34 -0700

    Added form resets to username and message fields

--- client/src/components/CreateMessageForm.tsx
+++ client/src/components/CreateMessageForm.tsx
@@ -3,28 +3,58 @@ import TimeStamp from 'time-stamp';
 
 export default function CreateMessageForm({ getMessages, socket }) {
   const [messageText, setMessageText] = useState('')
+  const [userNameInput, setUserNameInput] = useState('')
+  const [userName, setUserName] = useState(null)
 
   const submitNewMessage = () => {
     let time = (Number(TimeStamp('HH')) - 12) + ':' + TimeStamp('mm:ss');
 
     let newMessage = {
       userId: 0,
-      userName: 'Anonymous',
+      userName: userName || 'Anonymous',
       messageId: 0,
       timeStamp: time,
       messageContent: messageText
     }
     socket.emit('message', newMessage)
     getMessages();
+    setMessageText('');
   }
   return (
+    <>
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      setUserName(userNameInput)
+      e.target.reset();
+    }}>
+      <input
+        type="text"
+        placeholder="Enter Username"
+        onChange={e => setUserNameInput(e.target.value)}
+      >
+
+      </input>
+      <button>
+        Submit
+      </button>
+    </form>
+
     <form onSubmit={(e) => {
       e.preventDefault();
       submitNewMessage();
+      e.target.reset();
     }}>
-      <input type="text" onChange={e => setMessageText(e.target.value)}></input>
-      <button>Submit</button>
+      <input
+        type="text"
+        placeholder="Enter Message"
+        onChange={e => setMessageText(e.target.value)}
+      >
+      </input>
+      <button>
+        Submit
+      </button>
     </form>
+    </>
   )
 }
 
